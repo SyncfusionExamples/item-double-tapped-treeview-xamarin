43fd537823f9fe95a5895e01c313ca0c9b3144c5
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-08-26 17:50:21 +0530

    XAMARIN-35884 - TreeView Double tapped sample added.

---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/Behavior/Behavior.cs
@@ -0,0 +1,31 @@
+﻿using Syncfusion.XForms.TreeView;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TreeViewXamarin
+{
+    public class Behavior : Behavior<ContentPage>
+    {
+        Syncfusion.XForms.TreeView.SfTreeView treeView;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            treeView = bindable.FindByName<SfTreeView>("treeView");
+            treeView.ItemDoubleTapped += TreeView_ItemDoubleTapped;
+        }
+        private void TreeView_ItemDoubleTapped(object sender, ItemDoubleTappedEventArgs e)
+        {
+            App.Current.MainPage.DisplayAlert("Item DoubleTapped", "TreeView item double tapped", "Close");
+        }
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            treeView.ItemDoubleTapped += TreeView_ItemDoubleTapped;
+        }
+    }
+}
+        
+    
+
